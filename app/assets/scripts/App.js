b62921c8b3d8fe9b1e7a1d650de63bccc1c49560
Jason Greenstreet <Jason Greenstreet>
2018-02-04 14:52:30 -0600

    Made navbar sticky and changes opacity on scrolling down

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -12,6 +12,9 @@ window.onload = function() {
 
 import MobileMenu from './modules/Menu';
 import Menu from './modules/Menu';
+import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 var menu = new Menu();
+var stickyHeader = new StickyHeader();
