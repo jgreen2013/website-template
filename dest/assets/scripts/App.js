3313169f615d92c07e77f671631473c178848d62
Jason Greenstreet <jason.greenstreet@jasperproducts.com>
2018-07-15 10:05:54 -0500

    removed old comments from app.js

--- dest/assets/scripts/App.js
+++ dest/assets/scripts/App.js
@@ -1,15 +1,3 @@
-/* This is the old node way of importing
-var Person = require('./modules/Person');
-*/
-
-/*
-import Person from './modules/Person';
-window.onload = function() {
-  var jason = new Person("Jason Greenstreet", "red");
-  jason.greet();
-};
-*/
-
 import MobileMenu from './modules/Menu';
 import Menu from './modules/Menu';
 import $ from 'jquery';
